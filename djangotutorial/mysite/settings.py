1e8c4177a8f41932e9c65392da343789686c33cf
Logan <loganamos@rocketmail.com>
2026-04-21 16:49:56 +0000

    settings.py fixes

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -74,9 +74,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": Path(os.environ.get("DATA_DIR", BASE_DIR)) / "db.sqlite3",
     }
 }
 
